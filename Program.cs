14daf04f2ae29bd0c8fe99b57b68c12fa5d1b50c
Christopher Newell <cbnewell@byui.edu>
2020-09-16 16:33:27 -0500

    Up until W1 Assignment Part 6

--- Program.cs
+++ Program.cs
@@ -8,15 +8,25 @@ namespace CIT365_F2020_NewellBrycen
         {
             string name;
             string location;
-            var date = DateTime.Now;
+            DateTime date = DateTime.Today;
+            string dateString;
+            DateTime Christmas = new DateTime(2020, 12, 25);
+            int daysToChristmas;
+
 
             name = "Brycen Newell";
             location = "Oklahoma City, Oklahoma";
-            
-            Console.WriteLine("My name is " + name + ".");
-            Console.WriteLine("\nI am from " + location + ".");
+            dateString = date.ToString("d");
+            daysToChristmas = ((TimeSpan)(Christmas - date)).Days;
 
-            
+            //Output for 3, 4, and 5
+            Console.WriteLine($"My name is {name}.");
+            Console.WriteLine($"\nI am from {location}.");
+            Console.WriteLine($"\nToday is {dateString}.");
+            Console.WriteLine($"\nThere are only {daysToChristmas} days left until Christmas!");
+
+            //Section 2.1 in C# Programming Yellow Book
+           
         }
     }
 }
