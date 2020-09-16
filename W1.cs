01729ba6b2d92e4b198fe879b42feb76ff8f6600
Christopher Newell <cbnewell@byui.edu>
2020-09-16 17:33:06 -0500

    Completion of W1 Assignment

--- W1.cs
+++ W1.cs
@@ -26,7 +26,31 @@ namespace CIT365_F2020_NewellBrycen
             Console.WriteLine($"\nThere are only {daysToChristmas} days left until Christmas!");
 
             //Section 2.1 in C# Programming Yellow Book
-           
+
+            double width, height, woodLength, glassArea;
+            string widthString, heightString;
+
+            //Provide appropriate text labels when requesting dimensional input
+
+            Console.WriteLine("\nWhat is the width in metres?");
+            widthString = Console.ReadLine();
+            width = double.Parse(widthString);
+            Console.WriteLine("You said that the width is: " + width + " metres.");
+
+            Console.WriteLine("\nWhat is the height in metres?");
+            heightString = Console.ReadLine();
+            height = double.Parse(heightString);
+            Console.WriteLine("You said that the height is: " + height + " metres.");
+
+
+            woodLength = 2 * (width + height) * 3.25;
+            glassArea = 2 * (width * height);
+
+            Console.WriteLine("\nThe length of the wood is " + woodLength + " feet.");
+            Console.WriteLine("\nThe area of the glass is " + glassArea + " square feet.");
+
+            Console.WriteLine("\nType any key to exit.");
+            _ = Console.ReadKey();
         }
     }
 }
