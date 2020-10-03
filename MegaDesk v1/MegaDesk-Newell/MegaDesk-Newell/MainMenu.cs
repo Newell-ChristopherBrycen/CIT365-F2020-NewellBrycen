844b98e6e853d1f72b7129829ade6132c293bbd4
Christopher Newell <cbnewell@byui.edu>
2020-10-02 19:15:49 -0500

    Mega Desk v1 Forms Created

---
+++ MegaDesk v1/MegaDesk-Newell/MegaDesk-Newell/MainMenu.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MegaDesk_Newell
+{
+    public partial class MainMenu : Form
+    {
+        public MainMenu()
+        {
+            InitializeComponent();
+        }
+
+      
+        private void MainMenu_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void AddQuoteButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ViewQuotesButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void SearchQuotesButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ExitButton_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
